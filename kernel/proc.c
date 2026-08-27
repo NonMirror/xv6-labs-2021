3924ef1e002ceee8875ae4835c1e33e504336221
nonmirror <x24n8t@gmail.com>
2026-08-27 11:13:22 +0800

    small patch to speed up syscalls

--- kernel/proc.c
+++ kernel/proc.c
@@ -163,7 +163,10 @@ freeproc(struct proc *p)
 {
   if(p->trapframe)
     kfree((void*)p->trapframe);
+  if (p->usyscall)
+    kfree((void*)p->usyscall);
   p->trapframe = 0;
+  p->usyscall = 0;
   if(p->pagetable) {
     proc_freepagetable(p->pagetable, p->sz);
   }
