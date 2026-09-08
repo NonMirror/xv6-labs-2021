debeed3ffaaf8be5713a14eb57d727157ba6d880
nonmirror <x24n8t@gmail.com>
2026-09-08 14:25:59 +0800

    riscv asm and backtrace done, add sys_sigalarm and sys_sigreturn in sysproc.c

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -55,6 +55,7 @@ sys_sbrk(void)
 uint64
 sys_sleep(void)
 {
+  backtrace();
   int n;
   uint ticks0;
 
@@ -94,4 +95,24 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+uint64 sys_sigreturn(void) {
+  return 0;
+}
+
+uint64 sys_sigalarm(void) {
+  int ticks;
+  void (*handler)();
+  if (argint(0, &ticks) < 0) {
+    return -1;
+  }
+  if (argaddr(1, (uint64*)&handler) < 0) {
+    return -1;
+  }
+
+  struct proc *p = myproc();
+  p->ticks = ticks;
+  p->handler = handler;
+  return 0;
 }
